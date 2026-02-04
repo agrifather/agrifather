9a76d51a148200ad49b1fb4b15358e40140c74bf
Rakesh kumar <rakesh9803646587@gmail.com>
2026-02-04 12:53:12 +0530

    otp

--- server/utils/sendEmailOtp.js
+++ server/utils/sendEmailOtp.js
@@ -2,16 +2,18 @@ import nodemailer from "nodemailer";
 
 export const sendEmailOtp = async (email, otp) => {
   const transporter = nodemailer.createTransport({
+    // host: process.env.SMTP_HOST, // uses smtp.gmail.com
+    // port: process.env.SMTP_PORT, // uses 587
+    // secure: process.env.SMTP_SECURE === "true",
 
-    host: process.env.SMTP_HOST, // uses smtp.gmail.com
-    port: process.env.SMTP_PORT, // uses 587
-    secure: process.env.SMTP_SECURE === "true",
-    
     service: "gmail",
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
+    connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000,
   });
 
   await transporter.sendMail({
